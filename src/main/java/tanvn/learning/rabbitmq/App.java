53e6a513e5612d335ff331071158e46b572719d7
Vu Nhat Tan <vunhattan84@gmail.com>
2018-04-28 00:36:41 +0900

    test ssh commit

--- src/main/java/tanvn/learning/rabbitmq/App.java
+++ src/main/java/tanvn/learning/rabbitmq/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World RabbitMQ !" );
         (new Thread(new Recv())).run();
         (new Thread(new Send())).run();
     }
